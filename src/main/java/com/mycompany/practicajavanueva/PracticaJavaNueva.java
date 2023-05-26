e6499e16945e2887f728906df16a155c4ce17eea
Nicoogf <nicolasgfalabella@gmail.com>
2023-05-26 16:01:52 -0300

    Segunda prueba

--- src/main/java/com/mycompany/practicajavanueva/PracticaJavaNueva.java
+++ src/main/java/com/mycompany/practicajavanueva/PracticaJavaNueva.java
@@ -1,16 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.practicajavanueva;
 
-/**
- *
- * @author AMD
- */
+
 public class PracticaJavaNueva {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola Mundo");
     }
 }
