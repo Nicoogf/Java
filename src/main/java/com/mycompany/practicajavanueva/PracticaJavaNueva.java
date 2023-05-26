1d1d82315892141edfe0ac4ef0509ac1abcf790f
Nicoogf <nicolasgfalabella@gmail.com>
2023-05-26 16:00:04 -0300

    Inicio de Proyecto

---
+++ src/main/java/com/mycompany/practicajavanueva/PracticaJavaNueva.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicajavanueva;
+
+/**
+ *
+ * @author AMD
+ */
+public class PracticaJavaNueva {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
